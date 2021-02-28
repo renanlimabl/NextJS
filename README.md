898eaea69127feda0073cf1efece1ddbe05853a8
Renan Lima <renanlimabl@gmail.com>
2021-02-28 16:02:02 -0300

    Initial commit from Create Next App

---
+++ README.md
@@ -0,0 +1,17 @@
+# Boilerplate
+- [x] ReactJS
+- [x] TypeScript
+- [x] NextJS
+- [x] Storybook
+- [x] Jest
+- [x] Testing-library
+- [x] Eslint
+- [x] Prettier
+- [x] Pwa Support (next-pwa)
+- [x] styled-components
+- [x] babel
+
+### To use this:
+```
+yarn create next-app -e
+```
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  args: {
+    title: 'Title default',
+    description: 'Description default',
+  },
+} as Meta
+
+export const Default: Story = (args) => <Main {...args} />
+
+export const Basic: Story = (args) => <Main {...args} />
+
+Basic.args = {
+  title: 'Sobrescrevendo',
+  description: 'Sobrescrevendo description',
+}
